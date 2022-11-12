2f2aeed2da76dbfff962128c1832da3912ad35ec
Madhawa Monarawila <madawammonarawila@gmail.com>
2022-11-12 13:38:57 +0530

    Init the Chakra UI project with 3 pages

---
+++ src/feed.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Box } from "@chakra-ui/react";
+
+const Feed = () => {
+    return (
+        <Box maxWidth='1200px' mx='auto'>
+
+        </Box>
+    );
+}
+
+export { Feed };
