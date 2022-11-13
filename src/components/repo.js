9e904ff44d92d964857b4a9e9f73fd49b4bffc7d
Madhawa Monarawila <madawammonarawila@gmail.com>
2022-11-13 16:12:31 +0530

    Organize UIs in repo component

--- src/components/repo.js
+++ src/components/repo.js
@@ -7,43 +7,59 @@ export const Repo = (props) => {
     const { isListViewSet = false, repo } = props;
 
     return (
-        <Flex borderWidth={'1px'} p='15px' bg={'white'} rounded={'5px'} alignItems='center'>
-            <Flex flex={1} flexDir='column'>
+        <Flex borderWidth={'1px'} p='15px' bg={'white'} rounded={'5px'} position={'relative'}>
+            <Flex flex={1} flexDir='column' >
                 
-                {!isListViewSet && (
-                    <Flex mb={'20px'}>
-                        <Image src={repo.owner.avatar_url} width={'40px'} height={'40px'} rounded={100}>
-                        </Image>
-                        <Box ml={'10px'}>
-                            <Heading fontSize={'18px'}>{repo.owner.login}</Heading>
-                            <Text fontSize={'13px'} as='a' href={repo.owner.html_url} target='_blank'>View Profile</Text>
-                        </Box>
-                    </Flex>
-                )}
-                
-                <Box mb={'15px'}>
-                    <Box mb={'10px'}>
+                <Flex flexDir={'column'}>
+                    {!isListViewSet && (
+                        <Flex mb={'10px'} >
+                            <Image src={repo.owner.avatar_url} width={'40px'} height={'40px'} rounded={100}>
+                            </Image>
+                            <Box ml={'10px'}>
+                                <Heading fontSize={'18px'}>{repo.owner.login}</Heading>
+                                <Text fontSize={'13px'} as='a' href={repo.owner.html_url} target='_blank'>View Profile</Text>
+                            </Box>
+                        </Flex>
+                    )}
+
+                    <Flex mb={'15px'} flexDir={'column'} mt={'5px'}>
                         {isListViewSet && (
-                            <Stack isInline color='purple.700'>
+                            <Stack isInline color='purple.700' >
                                 <Heading as='a' href={repo.owner.html_url} target={'_blank'} fontSize={'23px'}>{repo.owner.login} /</Heading>
-                                <Heading as='a' href={repo.html_url} target={'_blank'} fontSize={'23px'}>{repo.name}</Heading>
+                                <Heading maxWidth={'50vw'} as='a' href={repo.html_url} target={'_blank'} fontSize={'23px'}>{repo.name}</Heading>
                             </Stack>
                         )}
                         {!isListViewSet && (
-                            <Heading as='a' href={repo.html_url} target={'_blank'} color='purple.700' fontSize={'22px'}>{repo.name}</Heading>
+                            <Box>
+                                <Heading mb={'10px'} as='a' href={repo.html_url} target={'_blank'} color='purple.700' fontSize={'22px'}>{repo.name}</Heading>
+                            </Box>
                         )}
-                        <Text color={'gray.600'} fontSize={'15px'}>
-                            Created at &middot; {moment(repo.created_at).format("MMMM D, YYYY")}
-                        </Text>
-                    </Box>
-                    <Text color={'gray.800'} fontSize={'15px'}>{repo.description}</Text>
-                </Box>
-                <Stack isInline>
+                        {isListViewSet && (
+                            <Box >
+                                <Text mb={'10px'} color={'gray.600'} fontSize={'15px'}>
+                                    Created at &middot; {moment(repo.created_at).format("MMMM D, YYYY")}
+                                </Text>
+                                <Text color={'gray.800'} fontSize={'15px'}>{repo.description}</Text>
+                            </Box>
+                        )}
+                        {!isListViewSet && (
+                            <Box mb={'25px'}>
+                                <Text mb={'10px'} color={'gray.600'} fontSize={'15px'}>
+                                    Created at &middot; {moment(repo.created_at).format("MMMM D, YYYY")}
+                                </Text>
+                                <Text color={'gray.800'} fontSize={'15px'}>{repo.description}</Text>
+                            </Box>
+                        )}
+                    </Flex>
+                </Flex>
+
+                <Stack isInline position={'absolute'} bottom='15px'>
                     <Button as={'a'} cursor={'pointer'} leftIcon={<GoStar />} variant='link' iconSpacing={'4px'} fontSize='15px' _hover={{ textDecor: 'none' }}>{repo.stargazers_count}</Button>
                     <Button as={'a'} cursor={'pointer'} leftIcon={<GoRepoForked />} variant='link' iconSpacing={'4px'} fontSize='15px' _hover={{ textDecor: 'none' }}>{repo.forks_count}</Button>
                     <Button as={'a'} cursor={'pointer'} leftIcon={<GoIssueOpened />} variant='link' iconSpacing={'4px'} fontSize='15px' _hover={{ textDecor: 'none' }}>{repo.open_issues_count}</Button>
                 </Stack>
             </Flex>
+            
             {isListViewSet && <Image src={repo.owner.avatar_url} width={'130px'} height={'130px'} rounded={'100%'}>
             </Image>}
         </Flex>
