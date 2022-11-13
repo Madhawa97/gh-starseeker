8ba00170e1bcc299f9417b19a881823bccb7e5d1
Madhawa Monarawila <madawammonarawila@gmail.com>
2022-11-13 13:57:15 +0530

    Update repo component to get content from API

--- src/components/repo.js
+++ src/components/repo.js
@@ -1,6 +1,7 @@
 import { Text, Box, Flex, Heading, Image, Stack, Button } from "@chakra-ui/react";
 import React from "react";
 import { GoStar, GoIssueOpened, GoRepoForked } from "react-icons/go";
+import moment from "moment";
 
 export const Repo = (props) => {
     const { isListViewSet = false, repo } = props;
@@ -11,11 +12,11 @@ export const Repo = (props) => {
                 
                 {!isListViewSet && (
                     <Flex mb={'20px'}>
-                        <Image src="https://www.w3schools.com/howto/img_avatar.png" width={'40px'} height={'40px'} rounded={'5px'}>
+                        <Image src={repo.owner.avatar_url} width={'40px'} height={'40px'} rounded={100}>
                         </Image>
                         <Box ml={'10px'}>
-                            <Heading fontSize={'18px'}>Madhawa97</Heading>
-                            <Text fontSize={'14px'}>View Profile</Text>
+                            <Heading fontSize={'18px'}>{repo.owner.login}</Heading>
+                            <Text fontSize={'13px'} as='a' href={repo.owner.html_url} target='_blank'>View Profile</Text>
                         </Box>
                     </Flex>
                 )}
@@ -23,25 +24,27 @@ export const Repo = (props) => {
                 <Box mb={'15px'}>
                     <Box mb={'10px'}>
                         {isListViewSet && (
-                            <Stack isInline as='a' href="https://github.com/Madhawa97/ssd-life-calc" target={'_blank'} color='purple.700'>
-                                <Heading fontSize={'22px'}>Madhawa97 /</Heading>
-                                <Heading fontSize={'22px'}>SSD-life-calc</Heading>
+                            <Stack isInline color='purple.700'>
+                                <Heading as='a' href={repo.owner.html_url} target={'_blank'} fontSize={'23px'}>{repo.owner.login} /</Heading>
+                                <Heading as='a' href={repo.html_url} target={'_blank'} fontSize={'23px'}>{repo.name}</Heading>
                             </Stack>
                         )}
                         {!isListViewSet && (
-                            <Heading as='a' href="https://github.com/Madhawa97/ssd-life-calc" target={'_blank'} color='purple.700' fontSize={'22px'}>SSD-life-calc</Heading>
+                            <Heading as='a' href={repo.html_url} target={'_blank'} color='purple.700' fontSize={'22px'}>{repo.name}</Heading>
                         )}
-                        <Text color={'gray.600'}>Meta info</Text>
+                        <Text color={'gray.600'} fontSize={'15px'}>
+                            Created at &middot; {moment(repo.created_at).format("MMMM D, YYYY")}
+                        </Text>
                     </Box>
-                    <Text color={'gray.800'}>Some repository description</Text>
+                    <Text color={'gray.800'} fontSize={'15px'}>{repo.description}</Text>
                 </Box>
                 <Stack isInline>
-                    <Button as={'a'} cursor={'pointer'} leftIcon={<GoStar />} variant='link' iconSpacing={'4px'} fontSize='15px' _hover={{ textDecor: 'none' }}>50</Button>
-                    <Button as={'a'} cursor={'pointer'} leftIcon={<GoRepoForked />} variant='link' iconSpacing={'4px'} fontSize='15px' _hover={{ textDecor: 'none' }}>Fork</Button>
-                    <Button as={'a'} cursor={'pointer'} leftIcon={<GoIssueOpened />} variant='link' iconSpacing={'4px'} fontSize='15px' _hover={{ textDecor: 'none' }}>Issue</Button>
+                    <Button as={'a'} cursor={'pointer'} leftIcon={<GoStar />} variant='link' iconSpacing={'4px'} fontSize='15px' _hover={{ textDecor: 'none' }}>{repo.stargazers_count}</Button>
+                    <Button as={'a'} cursor={'pointer'} leftIcon={<GoRepoForked />} variant='link' iconSpacing={'4px'} fontSize='15px' _hover={{ textDecor: 'none' }}>{repo.forks_count}</Button>
+                    <Button as={'a'} cursor={'pointer'} leftIcon={<GoIssueOpened />} variant='link' iconSpacing={'4px'} fontSize='15px' _hover={{ textDecor: 'none' }}>{repo.open_issues_count}</Button>
                 </Stack>
             </Flex>
-            {isListViewSet && <Image src="https://www.w3schools.com/howto/img_avatar.png" width={'130px'} height={'130px'} rounded={'100%'}>
+            {isListViewSet && <Image src={repo.owner.avatar_url} width={'130px'} height={'130px'} rounded={'100%'}>
             </Image>}
         </Flex>
     );
